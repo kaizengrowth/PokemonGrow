5766659e00de8554157eaf5211e14e7eca60d762
Kai Lin Zhang <kaizen.agility@gmail.com>
2017-11-02 10:52:49 -0400

    -m "Heroku"

--- db/config.js
+++ db/config.js
@@ -6,18 +6,18 @@ const options = {
 
 const pgp = require('pg-promise')(options);
 
+let db;
+
 function setDatabase() {
     if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-        return pgp({
+        db = pgp({
             database: 'pokemon_grow',
             port: 5432,
             host: 'localhost',
         });
     } else if (process.env.NODE_ENV === 'production') {
-        return pgp(process.env.DATABASE_URL);
+        db = pgp(process.env.DATABASE_URL);
     }
 }
-
-const db = setDatabase();
 
 module.exports = db;
